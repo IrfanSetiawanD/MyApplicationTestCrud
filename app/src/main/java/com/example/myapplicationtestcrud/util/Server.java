552fccd49087e070e50dd440ec2831a48478b408
IrfanSetiawanD <setiawan.irfan89@gmail.com>
2024-12-06 23:53:02 +0700

    Initial commit

---
+++ app/src/main/java/com/example/myapplicationtestcrud/util/Server.java
@@ -0,0 +1,7 @@
+package com.example.myapplicationtestcrud.util;
+
+    /* 10.0.2.2 adalah IP Address localhost EMULATOR ANDROID STUDIO,
+    Ganti IP Address tersebut dengan IP Laptop Apabila di RUN di HP. HP dan Laptop
+harus 1 jaringan */
+public static final String URL = "http://192.168.31.32/android/biodata/";
+}
