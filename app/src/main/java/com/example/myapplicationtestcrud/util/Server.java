6c22df18d704649d756bbf845fc29d9aaa117257
IrfanSetiawanD <setiawan.irfan89@gmail.com>
2024-12-07 02:00:50 +0700

    Update gradle dan file konfigurasi untuk proyek

--- app/src/main/java/com/example/myapplicationtestcrud/util/Server.java
+++ app/src/main/java/com/example/myapplicationtestcrud/util/Server.java
@@ -1,7 +1,6 @@
 package com.example.myapplicationtestcrud.util;
 
-    /* 10.0.2.2 adalah IP Address localhost EMULATOR ANDROID STUDIO,
-    Ganti IP Address tersebut dengan IP Laptop Apabila di RUN di HP. HP dan Laptop
-harus 1 jaringan */
-public static final String URL = "http://192.168.31.32/android/biodata/";
+public class Server {
+    // IP Address emulator Android Studio untuk localhost laptop
+    public static final String URL = "http://10.0.2.2/android/biodata/";
 }
